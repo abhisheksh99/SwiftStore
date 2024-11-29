e4b050176249b24c387f30e4a4ed397e73a921c6
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-29 13:47:57 -0500

    Admin sidebar and header created

--- frontend/src/components/admin-view/AdminLayout.jsx
+++ frontend/src/components/admin-view/AdminLayout.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import AdminSidebar from './AdminSidebar'
 import AdminHeader from './AdminHeader'
+import AdminSideBar from './AdminSidebar'
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex min-h-screen w-full">
-    {/* admin sidebar */}
-    <AdminSidebar/>
-
-    <div className="flex flex-1 flex-col">
-      {/* admin header */}
-      <AdminHeader/>
-
-      <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
-        <Outlet />
-      </main>
+      {/* admin sidebar */}
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
+      <div className="flex flex-1 flex-col">
+        {/* admin header */}
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
     </div>
-  </div>
   )
 }
 
